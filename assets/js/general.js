003fcffbccc809a275ef945b6370ef34205fa11f
KrunalKaPatel <krunal9611@gmail.com>
2020-05-04 20:37:49 +0530

    Apna message

--- assets/js/general.js
+++ assets/js/general.js
@@ -8,4 +8,40 @@ $(document).ready(function(){
 		x[i].style.width = (27 * count) + "px";
 		count += 1;
 	}
+
+	/**/
+	/*$('.card-slider').slick({
+  		infinite: true,
+ 	 	slidesToShow: 3,
+  		slidesToScroll: 3
+	});*/
+
+	$('.card-slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 840,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 620,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				}
+			}
+		]
+	});
 });
